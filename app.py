3d1d8c86fc1a38360c9127bb6f431b95fd200db6
Edgar Li <liedgar7@gmail.com>
2015-10-05 17:18:24 -0400

    Changed json file attributes to Text fields instead of strings

--- app.py
+++ app.py
@@ -528,34 +528,63 @@ def buy():
     quantity = request.form['quantity']
     amount = request.form['amount']
     email = request.form['email']
-    user = User.query.get(email)
-    print user
 
-    customer = stripe.Customer.create(
-        email=email,
-        card=request.form['stripeToken']
-    )
+    try:
+      customer = stripe.Customer.create(
+          email=email,
+          card=request.form['stripeToken']
+      )
 
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency='usd',
-        description='Sani Checkout'
-    )
+      charge = stripe.Charge.create(
+          customer=customer.id,
+          amount=amount,
+          currency='usd',
+          description='Sani Checkout'
+      )
 
-    order = SaniOrder(
-        uuid=str(uuid.uuid4()),
-        user=user,
-        email=email,
-        quantity=quantity,
-        amount=amount
-    )
+      order = SaniOrder(
+          uuid=str(uuid.uuid4()),
+          email=email,
+          quantity=quantity,
+          amount=amount
+      )
 
-    db.session.add(order)
-    db.session.commit()
+      db.session.add(order)
+      db.session.commit()
 
-    return render_template('buy.html', amount=amount)
+      return render_template('buy.html', amount=amount)
+      pass
+    except stripe.error.CardError, e:
+      # Since it's a decline, stripe.error.CardError will be caught
+      body = e.json_body
+      err  = body['error']
 
+      print "Status is: %s" % e.http_status
+      print "Type is: %s" % err['type']
+      print "Code is: %s" % err['code']
+      # param is '' in this case
+      print "Param is: %s" % err['param']
+      print "Message is: %s" % err['message']
+    except stripe.error.RateLimitError, e:
+      # Too many requests made to the API too quickly
+      pass
+    except stripe.error.InvalidRequestError, e:
+      # Invalid parameters were supplied to Stripe's API
+      pass
+    except stripe.error.AuthenticationError, e:
+      # Authentication with Stripe's API failed
+      # (maybe you changed API keys recently)
+      pass
+    except stripe.error.APIConnectionError, e:
+      # Network communication with Stripe failed
+      pass
+    except stripe.error.StripeError, e:
+      # Display a very generic error to the user, and maybe send
+      # yourself an email
+      pass
+    except Exception, e:
+      # Something else happened, completely unrelated to Stripe
+      pass
 
 # Test the connection of MySQL
 @app.route('/testdb')
--- models.py
+++ models.py
@@ -4,74 +4,75 @@ import flask.ext.whooshalchemy as whooshalchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
 
-	__tablename__ = 'user'
-	user_id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(255))
-	email = db.Column(db.String(255), unique = True)
-	gender = db.Column(db.String(255))
-	act = db.Column(db.String(255))
-	weight = db.Column(db.Float)
-	height = db.Column(db.Integer)
-	goal = db.Column(db.String(255))
-	age = db.Column(db.Integer)
-	timestamp = db.Column(db.DateTime, index = True, default = datetime.now)
-	ingredientJson = db.Column(db.String(255))
-	# orders = db.relationship('SaniOrder', backref='user', lazy='dynamic')
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    gender = db.Column(db.String(255))
+    act = db.Column(db.String(255))
+    weight = db.Column(db.Float)
+    height = db.Column(db.Integer)
+    goal = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    ingredientJson = db.Column(db.Text)
+    # orders = db.relationship('SaniOrder', backref='user', lazy='dynamic')
 
-	calories = db.Column(db.Float)
-	protein = db.Column(db.Float)
-	fat = db.Column(db.Float)
-	carbs = db.Column(db.Float)
+    calories = db.Column(db.Float)
+    protein = db.Column(db.Float)
+    fat = db.Column(db.Float)
+    carbs = db.Column(db.Float)
 
-	brown = db.Column(db.Integer)
-	protein_blend = db.Column(db.Integer)
-	carb_blend = db.Column(db.Integer)
-	fat_blend = db.Column(db.Integer)
+    brown = db.Column(db.Integer)
+    protein_blend = db.Column(db.Integer)
+    carb_blend = db.Column(db.Integer)
+    fat_blend = db.Column(db.Integer)
 
-	deviation = db.Column(db.Float)
+    deviation = db.Column(db.Float)
 
-	nutrient = db.Column(db.String(255))
-	nutrient2 = db.Column(db.String(255))
-	nutrientMerge = db.Column(db.String(255))
+    nutrient = db.Column(db.Text)
+    nutrient2 = db.Column(db.Text)
+    nutrientMerge = db.Column(db.Text)
 
-	ratioJson1 = db.Column(db.String(255))
-	ratioJson2 = db.Column(db.String(255))
+    ratioJson1 = db.Column(db.Text)
+    ratioJson2 = db.Column(db.Text)
 
-	def __init__(self, name, email, gender, act, weight, height, goal, age, calories, protein,
-		fat, carbs, ingredientJson, brown, protein_blend, carb_blend, fat_blend
-		,deviation, nutrient, nutrient2, nutrientMerge, ratioJson1, ratioJson2):
-		self.name = name
-		self.email = email
-		self.gender = gender
-		self.act = act
-		self.weight = weight
-		self.height = height
-		self.goal = goal
-		self.age = age
-		self.calories = calories
-		self.protein = protein
-		self.fat = fat
-		self.carbs = carbs
-		self.ingredientJson = ingredientJson
-		self.brown = brown
-		self.protein_blend = protein_blend
-		self.carb_blend = carb_blend
-		self.fat_blend = fat_blend
-		self.deviation = deviation
-		self.nutrient = nutrient
-		self.nutrient2 = nutrient2
-		self.nutrientMerge = nutrientMerge
-		self.ratioJson1 = ratioJson1
-		self.ratioJson2 = ratioJson2
+    def __init__(self, name, email, gender, act, weight, height, goal, age, calories, protein,
+                 fat, carbs, ingredientJson, brown, protein_blend, carb_blend, fat_blend, deviation, nutrient, nutrient2, nutrientMerge, ratioJson1, ratioJson2):
+        self.name = name
+        self.email = email
+        self.gender = gender
+        self.act = act
+        self.weight = weight
+        self.height = height
+        self.goal = goal
+        self.age = age
+        self.calories = calories
+        self.protein = protein
+        self.fat = fat
+        self.carbs = carbs
+        self.ingredientJson = ingredientJson
+        self.brown = brown
+        self.protein_blend = protein_blend
+        self.carb_blend = carb_blend
+        self.fat_blend = fat_blend
+        self.deviation = deviation
+        self.nutrient = nutrient
+        self.nutrient2 = nutrient2
+        self.nutrientMerge = nutrientMerge
+        self.ratioJson1 = ratioJson1
+        self.ratioJson2 = ratioJson2
+
 
 class SaniOrder(db.Model):
-	__tablename__ = 'saniorder'
-	uuid = db.Column(db.String(255), primary_key = True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-	user = db.relationship(User)
-	email = db.Column(db.String(255))
-	amount = db.Column(db.Integer)
-	quantity = db.Column(db.Integer)
-	subscription = db.Column(db.Boolean, default=False)
+    __tablename__ = 'saniorder'
+    uuid = db.Column(db.String(255), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user = db.relationship(User)
+    email = db.Column(db.String(255))
+    amount = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    subscription = db.Column(db.Boolean, default=False)
